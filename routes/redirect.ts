7169f2398b2fc2ec7fb3495272d66f6f38c7f3bf
merveesari <59048257+merveesari@users.noreply.github.com>
2023-10-22 05:20:09 +0300

    Issue Fix 7

--- routes/redirect.ts
+++ routes/redirect.ts
@@ -3,31 +3,29 @@
  * SPDX-License-Identifier: MIT
  */
 
-import utils = require('../lib/utils')
-import challengeUtils = require('../lib/challengeUtils')
-import { type Request, type Response, type NextFunction } from 'express'
+import { Request, Response, NextFunction } from 'express'
+import challengeUtils from '../lib/challengeUtils'
+import security from '../lib/insecurity'
+import { isSafeURL } from '../lib/utils'
+import { isURL } from 'validator'
 
-const security = require('../lib/insecurity')
-const challenges = require('../data/datacache').challenges
+module.exports = function performRedirect() {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const toUrl = req.query.to
 
-module.exports = function performRedirect () {
-  return ({ query }: Request, res: Response, next: NextFunction) => {
-    const toUrl = query.to
-    if (security.isRedirectAllowed(toUrl)) {
-      challengeUtils.solveIf(challenges.redirectCryptoCurrencyChallenge, () => { return toUrl === 'https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW' || toUrl === 'https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm' || toUrl === 'https://etherscan.io/address/0x0f933ab9fcaaa782d0279c300d73750e1311eae6' })
-      challengeUtils.solveIf(challenges.redirectChallenge, () => { return isUnintendedRedirect(toUrl as string) })
-      res.redirect(toUrl as string)
+    if (isURL(toUrl) && isSafeURL(toUrl)) {
+      challengeUtils.solveIf(challenges.redirectCryptoCurrencyChallenge, () => {
+        return (
+          toUrl ===
+          'https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW' ||
+          toUrl === 'https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm' ||
+          toUrl === 'https://etherscan.io/address/0x0f933ab9fcaaa782d0279c300d73750e1311eae6'
+        )
+      })
+      res.redirect(toUrl)
     } else {
       res.status(406)
-      next(new Error('Unrecognized target URL for redirect: ' + toUrl))
+      next(new Error('Unrecognized or unsafe target URL for redirect'))
     }
   }
-}
-
-function isUnintendedRedirect (toUrl: string) {
-  let unintended = true
-  for (const allowedUrl of security.redirectAllowlist) {
-    unintended = unintended && !utils.startsWith(toUrl, allowedUrl)
-  }
-  return unintended
 }
